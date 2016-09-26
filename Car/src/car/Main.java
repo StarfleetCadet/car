5419cf66868f0b60d6ef7324c4bf4dc741f3aae5
AD <meine@email.de>
2016-09-26 10:20:31 +0200

    practise, practise, practise

--- Car/src/car/Main.java
+++ Car/src/car/Main.java
@@ -30,7 +30,7 @@ public class Main extends Application {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        launch(args);
-    }
+//    public static void main(String[] args) {
+//        launch(args);
+   // }
 }
